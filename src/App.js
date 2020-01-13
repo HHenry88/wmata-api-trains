c0084df02a0f528e39771bbf59d55b1a306793e6
Henry Han <hanhenry88@gmail.com>
2020-01-13 22:33:27 +0800

    Add Table component to main App file with column and data props
    Use filterable columns for Table props. Get train data from api on load
    and 10 seconds afterwards. Add WMATA image to App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from 'react'
-import './App.css'
+import React, { useEffect, useMemo, useState } from 'react'
 
-function App() {
+import './App.css'
+import getTrainPositions from './utils/getTrainPositions.js'
+import Table from './components/Table'
+import {
+  NumberRangeColumnFilter,
+  SelectColumnFilter,
+  DefaultColumnFilter,
+} from './utils/filters'
+
+import logo from './images/WMATAlogo.png'
+
+const App = () => {
+  const [trainData, setTrainData] = useState([])
+
+  useEffect(() => {
+    // Initiate get trains on app load, and then every 10 seconds afterwards.
+    const fetchData = async isForUpdate => {
+      const trainPositions = await getTrainPositions()
+      if (isForUpdate) {
+      }
+      setTrainData(trainPositions.data.TrainPositions)
+    }
+
+    fetchData()
+
+    // Auto update Train positions. WMATA data is updated every 7-10 seconds.
+
+    setInterval(() => {
+      fetchData(true)
+    }, 10000)
+  }, [])
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Train ID',
+        accessor: 'TrainId',
+        Filter: DefaultColumnFilter,
+      },
+      {
+        Header: 'Train Number',
+        accessor: 'TrainNumber',
+        Filter: DefaultColumnFilter,
+      },
+      {
+        Header: 'Car Count',
+        accessor: 'CarCount',
+        Filter: NumberRangeColumnFilter,
+        filter: 'between',
+      },
+      {
+        Header: 'Service Type',
+        accessor: 'ServiceType',
+        Filter: SelectColumnFilter,
+        filter: 'includes',
+      },
+      {
+        Header: 'Line Color',
+        accessor: 'LineCode',
+        Filter: SelectColumnFilter,
+        filter: 'includes',
+      },
+    ],
+    [],
+  )
+
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="WMATA logo" className="img-wmatalogo" />
         <h1 className="header-title">WMATA Trains</h1>
       </header>
+      <Table columns={columns} data={trainData} />
     </div>
   )
 }
