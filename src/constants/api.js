9fd52c60a41453822cb1ac8607473b9fbedeb21e
Henry Han <hanhenry88@gmail.com>
2020-01-13 17:08:39 +0800

    Constants for WMATA api

---
+++ src/constants/api.js
@@ -0,0 +1,5 @@
+// Demo key that's available at https://developer.wmata.com/Products
+export const demoAPIKey = 'e13626d03d8e4c03ac07f95541b3091b'
+
+export const trainURL = 'https://api.wmata.com/TrainPositions/TrainPositions'
+export const contentType = 'json'
